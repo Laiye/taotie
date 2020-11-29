c6d5ad7f259ba6c66c0a70ef7a48fe511e42beae
BlackCyclone <564844781@qq.com>
2020-11-29 17:16:44 +0800

    处理中文编码问题

--- src/main/java/com/bizarreanimals/taotie/api/v1/BannerController.java
+++ src/main/java/com/bizarreanimals/taotie/api/v1/BannerController.java
@@ -1,5 +1,6 @@
 package com.bizarreanimals.taotie.api.v1;
 
+import com.bizarreanimals.taotie.exception.http.NotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ public class BannerController {
 
     @GetMapping("/test")
     public String test() {
-        return "Hello, 奥利奥";
+//        return "Hello, 奥利奥";
+        throw new NotFoundException(10000);
     }
 }
