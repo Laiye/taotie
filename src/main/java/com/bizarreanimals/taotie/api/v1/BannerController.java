53d125cce7d1796ef955fcb16a67d2c6ea3e6c9b
BlackCyclone <564844781@qq.com>
2020-11-29 21:06:04 +0800

    参数传递

--- src/main/java/com/bizarreanimals/taotie/api/v1/BannerController.java
+++ src/main/java/com/bizarreanimals/taotie/api/v1/BannerController.java
@@ -1,17 +1,37 @@
 package com.bizarreanimals.taotie.api.v1;
 
 import com.bizarreanimals.taotie.exception.http.NotFoundException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/v1/banner")
 public class BannerController {
 
+    /**
+     * 测试自定义异常及统一消息处理
+     * @return
+     */
     @GetMapping("/test")
     public String test() {
 //        return "Hello, 奥利奥";
         throw new NotFoundException(10000);
+    }
+
+    /**
+     * 测试URL参数传递
+     * {id} 路径参数 @PathVariable
+     * ？name=abc 查询参数 @RequestParam
+     * @return
+     */
+    @GetMapping("/test/parameter/path/{id}")
+    public String testParameterPath(@PathVariable Integer id, @RequestParam String name) {
+        return "testParameter";
+    }
+
+    @PostMapping("/test/parameter/body")
+    public String testParameterBody(@RequestBody Map<String, Object> person) {
+        return "testParameterBody";
     }
 }
