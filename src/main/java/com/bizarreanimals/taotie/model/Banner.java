4c24bb5176dfa24ec2f50ca20d02cf89bb9e31e1
BlackCyclone <564844781@qq.com>
2020-11-30 22:02:42 +0800

    新建模型类，对应数据库中的一张表

---
+++ src/main/java/com/bizarreanimals/taotie/model/Banner.java
@@ -0,0 +1,12 @@
+package com.bizarreanimals.taotie.model;
+
+public class Banner {
+
+    private long id;
+    private String name;
+    private String description;
+    private String img;
+    private String title;
+}
+
+// 让模型类变成数据库中的一张表
