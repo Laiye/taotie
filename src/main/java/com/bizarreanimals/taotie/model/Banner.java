a8dd063b3d2599856139f845408fa811fc013309
BlackCyclone <564844781@qq.com>
2020-12-01 20:28:35 +0800

    使用ORM的方式，通过模型Entity生成数据表

--- src/main/java/com/bizarreanimals/taotie/model/Banner.java
+++ src/main/java/com/bizarreanimals/taotie/model/Banner.java
@@ -1,7 +1,12 @@
 package com.bizarreanimals.taotie.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Banner {
 
+    @Id
     private long id;
     private String name;
     private String description;
