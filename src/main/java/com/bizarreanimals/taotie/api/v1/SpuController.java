11589306e14ef24c735c10e10b91ba23b628f5bf
BlackCyclone <564844781@qq.com>
2020-12-09 20:35:43 +0800

    建立VO对象，引入DozerMapper库

--- src/main/java/com/bizarreanimals/taotie/api/v1/SpuController.java
+++ src/main/java/com/bizarreanimals/taotie/api/v1/SpuController.java
@@ -3,6 +3,10 @@ package com.bizarreanimals.taotie.api.v1;
 import com.bizarreanimals.taotie.exception.http.NotFoundException;
 import com.bizarreanimals.taotie.model.Spu;
 import com.bizarreanimals.taotie.service.SpuService;
+import com.bizarreanimals.taotie.vo.SpuSimplifyVO;
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Positive;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -32,7 +37,22 @@ public class SpuController {
     }
 
     @GetMapping("/latest")
-    public List<Spu> getLatestSpuList() {
-        return this.spuService.getLatestPagingSpu();
+    public List<SpuSimplifyVO> getLatestSpuList() {
+        Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+        List<Spu> spuList = this.spuService.getLatestPagingSpu();
+        List<SpuSimplifyVO> vos = new ArrayList<>();
+        spuList.forEach( s -> {
+            SpuSimplifyVO vo = mapper.map(s, SpuSimplifyVO.class);
+            vos.add(vo);
+        });
+        return vos;
+    }
+
+    @GetMapping("/id/{id}/simplify")
+    public SpuSimplifyVO getSimplifySpu(@PathVariable @Positive Long id) {
+        Spu spu = this.spuService.getSpu(id);
+        SpuSimplifyVO vo = new SpuSimplifyVO();
+        BeanUtils.copyProperties(spu, vo);
+        return vo;
     }
 }
